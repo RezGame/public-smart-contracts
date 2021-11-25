475ae9ed2d49f7a0353a163c141f13e810078b03
rpmaya <35469068+rpmaya@users.noreply.github.com>
2021-11-25 18:59:25 +0100

    market contract changed

--- BinanceSmartChain/RezMarket.sol
+++ BinanceSmartChain/RezMarket.sol
@@ -1,14 +1,24 @@
+// contracts/Market.sol
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract RezMarket is ReentrancyGuard {
+//import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
   using Counters for Counters.Counter;
   Counters.Counter private _itemIds;
   Counters.Counter private _itemsSold;
+
+  address payable owner;
+  uint256 listingPrice = 0.025 ether;
+
+  constructor() {
+    owner = payable(msg.sender);
+  }
 
   struct MarketItem {
     uint itemId;
@@ -17,6 +27,7 @@ contract RezMarket is ReentrancyGuard {
     address payable seller;
     address payable owner;
     uint256 price;
+    bool sold;
   }
 
   mapping(uint256 => MarketItem) private idToMarketItem;
@@ -27,19 +38,23 @@ contract RezMarket is ReentrancyGuard {
     uint256 indexed tokenId,
     address seller,
     address owner,
-    uint256 price
+    uint256 price,
+    bool sold
   );
 
-  function getMarketItem(uint256 marketItemId) public view returns (MarketItem memory) {
-    return idToMarketItem[marketItemId];
+  /* Returns the listing price of the contract */
+  function getListingPrice() public view returns (uint256) {
+    return listingPrice;
   }
-
+  
+  /* Places an item for sale on the marketplace */
   function createMarketItem(
     address nftContract,
     uint256 tokenId,
     uint256 price
   ) public payable nonReentrant {
     require(price > 0, "Price must be at least 1 wei");
+    require(msg.value == listingPrice, "Price must be equal to listing price");
 
     _itemIds.increment();
     uint256 itemId = _itemIds.current();
@@ -50,7 +65,8 @@ contract RezMarket is ReentrancyGuard {
       tokenId,
       payable(msg.sender),
       payable(address(0)),
-      price
+      price,
+      false
     );
 
     IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
@@ -61,10 +77,13 @@ contract RezMarket is ReentrancyGuard {
       tokenId,
       msg.sender,
       address(0),
-      price
+      price,
+      false
     );
   }
 
+  /* Creates the sale of a marketplace item */
+  /* Transfers ownership of the item, as well as funds between parties */
   function createMarketSale(
     address nftContract,
     uint256 itemId
@@ -76,14 +95,12 @@ contract RezMarket is ReentrancyGuard {
     idToMarketItem[itemId].seller.transfer(msg.value);
     IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
     idToMarketItem[itemId].owner = payable(msg.sender);
+    idToMarketItem[itemId].sold = true;
     _itemsSold.increment();
+    payable(owner).transfer(listingPrice);
   }
 
-  function fetchMarketItem(uint itemId) public view returns (MarketItem memory) {
-    MarketItem memory item = idToMarketItem[itemId];
-    return item;
-  }
-
+  /* Returns all unsold market items */
   function fetchMarketItems() public view returns (MarketItem[] memory) {
     uint itemCount = _itemIds.current();
     uint unsoldItemCount = _itemIds.current() - _itemsSold.current();
@@ -92,16 +109,16 @@ contract RezMarket is ReentrancyGuard {
     MarketItem[] memory items = new MarketItem[](unsoldItemCount);
     for (uint i = 0; i < itemCount; i++) {
       if (idToMarketItem[i + 1].owner == address(0)) {
-        uint currentId = idToMarketItem[i + 1].itemId;
+        uint currentId =  i + 1;
         MarketItem storage currentItem = idToMarketItem[currentId];
         items[currentIndex] = currentItem;
         currentIndex += 1;
       }
     }
-   
     return items;
   }
 
+  /* Returns only items that a user has purchased */
   function fetchMyNFTs() public view returns (MarketItem[] memory) {
     uint totalItemCount = _itemIds.current();
     uint itemCount = 0;
@@ -116,13 +133,36 @@ contract RezMarket is ReentrancyGuard {
     MarketItem[] memory items = new MarketItem[](itemCount);
     for (uint i = 0; i < totalItemCount; i++) {
       if (idToMarketItem[i + 1].owner == msg.sender) {
-        uint currentId = idToMarketItem[i + 1].itemId;
+        uint currentId =  i + 1;
         MarketItem storage currentItem = idToMarketItem[currentId];
         items[currentIndex] = currentItem;
         currentIndex += 1;
       }
     }
-   
+    return items;
+  }
+
+  /* Returns only items a user has created */
+  function fetchItemsCreated() public view returns (MarketItem[] memory) {
+    uint totalItemCount = _itemIds.current();
+    uint itemCount = 0;
+    uint currentIndex = 0;
+
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        itemCount += 1;
+      }
+    }
+
+    MarketItem[] memory items = new MarketItem[](itemCount);
+    for (uint i = 0; i < totalItemCount; i++) {
+      if (idToMarketItem[i + 1].seller == msg.sender) {
+        uint currentId = i + 1;
+        MarketItem storage currentItem = idToMarketItem[currentId];
+        items[currentIndex] = currentItem;
+        currentIndex += 1;
+      }
+    }
     return items;
   }
 }
